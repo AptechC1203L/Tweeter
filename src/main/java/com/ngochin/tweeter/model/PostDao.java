0153908042057d8ce91f2bddb2b0bb99bd532b9e
Trung Ngo <ndtrung4419@gmail.com>
2014-09-09 02:01:48 +0700

    Implement database code for PostDao

--- src/main/java/com/ngochin/tweeter/model/PostDao.java
+++ src/main/java/com/ngochin/tweeter/model/PostDao.java
@@ -5,6 +5,12 @@
  */
 package com.ngochin.tweeter.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -15,45 +21,81 @@ import java.util.List;
  */
 public class PostDao {
 
-    private static ArrayList<Post> posts;
+    private String connectionString;
 
-    public PostDao() {
-        if (posts == null) {
-            posts = new ArrayList<Post>();
-            addPost(new Post("Post 1", "chin", new Date()));
-            addPost(new Post("Post 2", "chin", new Date()));
-        }
+    public PostDao(String connectionString) {
+        this.connectionString = connectionString;
     }
 
-    public PostDao addPost(Post p) {
-        p.setId(posts.size());
-        posts.add(p);
+    public PostDao addPost(Post p) throws SQLException {
+        try (Connection conn = DriverManager.getConnection(connectionString)) {
+            Statement st = conn.createStatement();
+            st.executeUpdate(
+                String.format("insert into posts (user_name, creation_time, content) "
+                        + "values (\"%s\", datetime(\"now\"), \"%s\")",
+                        p.getUsername(), p.getText()));
+        }
+        
         return this;
     }
+    
+    private Post postFromRs(ResultSet rs) throws SQLException {
+        Post p = new Post();
 
-    public Post getPost(int postId) {
-        for (Post p : posts) {
-            if (p.getId() == postId) {
-                return p;
+        int id = rs.getInt("id");
+        String userName = rs.getString("user_name");
+        Date creationTime = rs.getTime("creation_time");
+        String content = rs.getString("content");
+
+        p.setId(id);
+        p.setText(content);
+        p.setTimestamp(creationTime);
+        p.setUsername(userName);
+
+        return p;
+    }
+
+    public Post getPost(int postId) throws SQLException {
+        try (Connection conn = DriverManager.getConnection(connectionString)) {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select * from posts where id=" + Integer.toString(postId));
+            
+            while (rs.next()) {
+                return postFromRs(rs);
             }
         }
 
         return null;
     }
 
-    public List<Post> getPostsFromUser(String username) {
-        ArrayList<Post> postsByUser = new ArrayList<Post>();
+    public List<Post> getPostsFromUser(String username) throws SQLException {
+        ArrayList<Post> posts = new ArrayList<>();
 
-        for (Post p : posts) {
-            if (p.getUsername().equals(username)) {
-                postsByUser.add(p);
+        try (Connection conn = DriverManager.getConnection(connectionString)) {
+            Statement st = conn.createStatement();
+            ResultSet rs = 
+                st.executeQuery("select * from posts where user_name=" + username);
+
+            while (rs.next()) {
+                posts.add(postFromRs(rs));
             }
         }
 
-        return postsByUser;
+        return posts;
     }
 
-    public List<Post> getAllPosts() {
+    public List<Post> getAllPosts() throws SQLException {
+        ArrayList<Post> posts = new ArrayList<>();
+
+        try (Connection conn = DriverManager.getConnection(connectionString)) {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select * from posts");
+
+            while (rs.next()) {
+                posts.add(postFromRs(rs));
+            }
+        }
+
         return posts;
     }
 }
