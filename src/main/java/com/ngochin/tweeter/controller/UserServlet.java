a3dce90d189614bf74cb76c2eeeeb0b1ae450c38
Trung Ngo <ndtrung4419@gmail.com>
2014-09-09 14:12:48 +0700

    Remove unnecessary try-catch

--- src/main/java/com/ngochin/tweeter/controller/UserServlet.java
+++ src/main/java/com/ngochin/tweeter/controller/UserServlet.java
@@ -40,25 +40,21 @@ public class UserServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        try {
-            response.setContentType("text/html;charset=UTF-8");
-            
-            String userName = request.getPathInfo().substring(1);
-            
-            String dbUrl = getServletContext().getInitParameter("dbUrl");
-            DaoFactory daoFactory = new DaoFactory(dbUrl);
-            
-            User u = daoFactory.getUserDao().getUser(userName);
-            List<Post> posts = daoFactory.getPostDao().getPostsFromUser(userName);
-            
-            request.setAttribute("user", u);
-            request.setAttribute("posts", posts);
-            
-            RequestDispatcher rd = request.getRequestDispatcher("/user.jsp");
-            rd.forward(request, response);
-        } catch (SQLException ex) {
-            Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        response.setContentType("text/html;charset=UTF-8");
+
+        String userName = request.getPathInfo().substring(1);
+
+        String dbUrl = getServletContext().getInitParameter("dbUrl");
+        DaoFactory daoFactory = new DaoFactory(dbUrl);
+
+        User u = daoFactory.getUserDao().getUser(userName);
+        List<Post> posts = daoFactory.getPostDao().getPostsFromUser(userName);
+
+        request.setAttribute("user", u);
+        request.setAttribute("posts", posts);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/user.jsp");
+        rd.forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
