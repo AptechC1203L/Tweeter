b5267969e26551a6c9d55a72647056aa4b5537d3
Trung Ngo <ndtrung4419@gmail.com>
2014-09-09 09:54:43 +0700

    Create DaoFactory

---
+++ src/main/java/com/ngochin/tweeter/model/DaoFactory.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ngochin.tweeter.model;
+
+/**
+ *
+ * @author chin
+ */
+public class DaoFactory {
+    private final String connectionString;
+    
+    public DaoFactory(String connectionString) {
+        this.connectionString = connectionString;
+    }
+
+    public CommentDao getCommentDao() {
+        return new CommentDao(connectionString);
+    }
+    
+    public PostDao getPostDao() {
+        return new PostDao(connectionString);
+    }
+    
+    public UserDao getUserDao() {
+        return new UserDao();
+    }
+}
