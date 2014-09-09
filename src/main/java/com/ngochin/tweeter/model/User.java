e5200ea742d6268f5ee4f4d0ba46916052483011
Trung Ngo <ndtrung4419@gmail.com>
2014-09-09 11:37:08 +0700

    Prefer nullary constructor for User

--- src/main/java/com/ngochin/tweeter/model/User.java
+++ src/main/java/com/ngochin/tweeter/model/User.java
@@ -14,9 +14,7 @@ public class User {
     private String userId;
     private String fullName;
     
-    public User(String userId, String fullName) {
-        this.userId = userId;
-        this.fullName = fullName;
+    public User() {
     }
 
     public String getFullName() {
