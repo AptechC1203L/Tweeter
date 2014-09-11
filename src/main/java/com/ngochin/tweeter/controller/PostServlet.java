69840069a7207a1ce3beb1a7e2b8784f4a51f819
Trung Ngo <ndtrung4419@gmail.com>
2014-09-11 23:09:57 +0700

    Allow viewing individual posts

--- src/main/java/com/ngochin/tweeter/controller/PostServlet.java
+++ src/main/java/com/ngochin/tweeter/controller/PostServlet.java
@@ -9,6 +9,7 @@ package com.ngochin.tweeter.controller;
 import com.ngochin.tweeter.model.DaoFactory;
 import com.ngochin.tweeter.model.Post;
 import java.io.IOException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,17 +24,47 @@ import javax.servlet.http.HttpServletResponse;
 public class PostServlet extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
+     * Handles the HTTP <code>GET</code> method.
      *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+        String postId = request.getPathInfo();
+        System.out.println(postId);
+        
+        // The empty pathInfo is '/'
+        if (postId.length() > 1) {
+            try {
+                int id = Integer.parseInt(postId.substring(1));
+                Post post = new DaoFactory().getPostDao().getPost(id);
+                if (post != null) {
+                    request.setAttribute("post", post);
+
+                    RequestDispatcher rd = request.getRequestDispatcher("/single-post.jsp");
+                    rd.forward(request, response);
+                }
+            } catch (NumberFormatException e) {
+            }
+        }
+        response.sendRedirect(getServletContext().getContextPath());
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         String postContent = request.getParameter("postContent");
 
         if (!(postContent == null || postContent.isEmpty())) {
@@ -49,34 +80,6 @@ public class PostServlet extends HttpServlet {
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
     /**
      * Returns a short description of the servlet.
      *
---
+++ src/main/webapp/single-post.jsp
@@ -0,0 +1,12 @@
+<%-- 
+    Document   : single-post
+    Created on : Sep 11, 2014, 10:02:59 PM
+    Author     : chin
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="h" tagdir="/WEB-INF/tags"%>
+<!DOCTYPE html>
+<h:master title="Tweeter Login">
+    <h:post post="${post}"/>
+</h:master>
