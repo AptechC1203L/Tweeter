a757dbf252af24bc72dd865afdbafe6e21d18515
Trung Ngo <ndtrung4419@gmail.com>
2014-09-12 19:03:47 +0700

    Always interpret requests as UTF-8

--- src/main/java/com/ngochin/tweeter/filter/DefaultContentTypeFilter.java
+++ src/main/java/com/ngochin/tweeter/filter/DefaultContentTypeFilter.java
@@ -28,6 +28,7 @@ public class DefaultContentTypeFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=UTF-8");
         chain.doFilter(request,response);
     }
